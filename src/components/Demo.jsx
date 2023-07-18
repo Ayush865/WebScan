5b839dbd55d09e499d883de60a3bb387668115d7
Ayush Prakash <aayushman865@gmail.com>
2023-07-18 13:51:50 +0530

    submit icon

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -83,11 +83,25 @@ const Demo = () => {
             className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
           <button
+          type='submit'
+          className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <div class="svg-wrapper-1">
+              <div class="svg-wrapper">
+                <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
+                </svg>
+              </div>
+            </div>
+            <span>Send</span>
+          </button>
+          {/* <button
             type='submit'
             className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
           >
             <p>⫸</p>
-          </button>
+          </button> */}
         </form>
 
         {/* Browse History */}
